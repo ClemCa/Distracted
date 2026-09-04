4d05d4d493804d6f0d7a939b89c147244869943b
ClemCa <perso.catorc@gmail.com>
2026-09-04 18:30:15 -0400

    Renamed demo assets

--- src/main/java/com/clemca/distracted/Distracted.java
+++ src/main/java/com/clemca/distracted/Distracted.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class Distracted {
     public static void main(String[] args) {
-      SpringApplication.run(DemoApplication.class, args);
+      SpringApplication.run(Distracted.class, args);
     }
     @GetMapping("/health")
     public String health() {
